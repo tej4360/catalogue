b53e1a10333dab830feffa0d957eab191b7ad861
raviteja <teja4360@gmail.com>
2024-01-29 20:05:33 +0530

    Random txt

--- server.js
+++ server.js
@@ -131,7 +131,7 @@ app.get('/search/:text', (req, res) => {
 // set up Mongo
 function mongoConnect() {
     return new Promise((resolve, reject) => {
-        var mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/catalogue';
+        var mongoURL = process.env.MONGO_URL || 'mongodb://mongo-service:27017/catalogue';
         mongoClient.connect(mongoURL, (error, client) => {
             if(error) {
                 reject(error);
