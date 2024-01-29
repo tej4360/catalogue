57dfe826fbb57fdcfacf7d58c96fdaaac85c90af
raviteja <teja4360@gmail.com>
2024-01-29 21:26:39 +0530

    Random txt

--- dockerfile
+++ dockerfile
@@ -3,6 +3,8 @@ RUN         mkdir /app
 WORKDIR     /app
 COPY        server.js .
 COPY        package.json ./
+# Set environment variable for MONGO
+ENV         MONGO true
 RUN         npm install
 RUN         npm install mongodb@2.2.33
 ENTRYPOINT  [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -128,10 +128,11 @@ app.get('/search/:text', (req, res) => {
     }
 });
 
+if (process.env.MONGO == 'true') {
 // set up Mongo
 function mongoConnect() {
     return new Promise((resolve, reject) => {
-        var mongoURL = process.env.MONGO_URL || 'mongodb://mongo-service:27017/catalogue';
+        var mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/catalogue';
         mongoClient.connect(mongoURL, (error, client) => {
             if(error) {
                 reject(error);
@@ -143,7 +144,7 @@ function mongoConnect() {
         });
     });
 }
-
+}
 
 if (process.env.DOCUMENTDB == 'true') {
 function mongoConnect() {
